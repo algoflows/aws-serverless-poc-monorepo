86fecbddd71029cc1392fe775c1959d841277c61
Sean Knowles <s@seanknowles.dev>
2021-08-02 23:08:05 +0100

    testing cicd depl

--- logbooks/src/functions/diver/create-dive.js
+++ logbooks/src/functions/diver/create-dive.js
@@ -3,6 +3,8 @@ import createDiveSchema from "../../schema/createDiveSchema"
 import validator from "@middy/validator"
 import { v4 as uuid } from "uuid"
 
+// testing cicd
+
 export const DIVE_DIVER_TABLE = process.env.DIVE_DIVER_TABLE
 
 const main = lambdaHandler(async (event, context) => {
