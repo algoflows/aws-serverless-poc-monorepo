f5f7b27cdff27693bd020ae098fd1dc437e381c8
Sean Knowles <65465380+JuniorDEVed@users.noreply.github.com>
2021-08-02 23:03:25 +0100

    Delete shared directory

--- shared/index.js
+++
@@ -1,4 +0,0 @@
-export { dynamodb } from "./dynamodb-lib"
-export { lambdaHandler } from "./handler-lib"
-export { commonMiddleware } from "./commonMiddleware"
-export { createError } from "http-errors"
