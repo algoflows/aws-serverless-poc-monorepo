125f932a3e9c1e660e93f5a53830962932b457c9
Sean Knowles <s@seanknowles.dev>
2021-08-04 23:35:31 +0100

    pnpm workspaces added services name changes to dirs

---
+++ homefeeds-api/functions/diving/diver/create-entry.js
@@ -0,0 +1,87 @@
+import { lambdaHandler, dynamodb, commonMiddleware } from "../../../lib"
+import validator from "@middy/validator"
+import { v4 as uuid } from "uuid"
+import { Logbook } from "../logbookEnum"
+
+const LOGBOOK_SERVICE_TABLE = process.env.LOGBOOK_SERVICE_TABLE
+
+/* Schema validation fields - TODO
+    userId,
+    supervisorName,
+    supervisorEmail,
+    companyName,
+    clientName,
+    diveLocation,
+    maximumDepthMeters,
+    leftSurfaceAt,
+    bottomTimeMinutes,
+    decoCompletedAt,
+    tableUsed,
+    breathingMixture,
+    equipmentUsed,
+    diveDescription,
+*/
+
+const main = lambdaHandler(async (event, context) => {
+  const { userId } = event.body
+  const now = new Date()
+
+  const params = {
+    TableName: LOGBOOK_SERVICE_TABLE,
+    Item: {
+      ...event.body,
+      PK: `userId#${userId}`,
+      SK: `diveId#${uuid()}`,
+      LGBK: Logbook.diver,
+      userVerifiedAt: "false",
+      companyVerifiedAt: "false",
+      clientVerfiedAt: "false",
+      createdAt: now.toISOString(),
+      updatedAt: now.toISOString(),
+    },
+    ReturnValues: "ALL_OLD",
+  }
+
+  await dynamodb.put(params)
+
+  return params.Item
+})
+
+const schema = {
+  type: "object",
+  properties: {
+    body: {
+      type: "object",
+      properties: {
+        userId: {
+          type: "string",
+        },
+        verifierId: {
+          type: "string",
+        },
+        company: {
+          type: "string",
+        },
+        logType: {
+          type: "string",
+        },
+        country: {
+          type: "string",
+        },
+      },
+      required: ["userId", "verifierId", "company", "logType", "country"],
+    },
+  },
+  required: ["body"],
+}
+
+// exported lambda handler func with middleware
+export const handler = commonMiddleware(main).use(
+  validator({
+    inputSchema: schema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  })
+)
---
+++ homefeeds-api/functions/diving/logbookEnum.js
@@ -0,0 +1,6 @@
+export const Logbook = {
+  diver: "DIVING_DIVER",
+  supervisor: "DIVING_SUPERVISOR",
+  inspection: "DIVING_INSPECTION",
+  inspection_supervisor: "DIVING_INSPECTION_SUPERVISOR",
+}
