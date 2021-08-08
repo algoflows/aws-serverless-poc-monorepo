a4f940b9c870c05540bb4f62ff02f1249223f0b1
Sean Knowles <s@seanknowles.dev>
2021-08-08 23:36:42 +0100

    minor fixes to function name

--- services/auth-service/functions/public.js
+++ services/auth-service/functions/public.js
@@ -5,10 +5,10 @@ export async function handler(event, context) {
       /* Required for CORS support to work */
       'Access-Control-Allow-Origin': '*',
       /* Required for cookies, authorization headers with HTTPS */
-      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Credentials': true
     },
     body: JSON.stringify({
-      message: 'Hi ⊂◉‿◉つ from Public API',
-    }),
-  };
+      message: 'Hi ⊂◉‿◉つ from Public API'
+    })
+  }
 }
