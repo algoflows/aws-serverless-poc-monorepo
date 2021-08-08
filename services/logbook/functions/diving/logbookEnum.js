598b5c5b284adc334b4e3f6fd6f15e1a56455d06
Sean Knowles <65465380+JuniorDEVed@users.noreply.github.com>
2021-08-08 23:20:53 +0100

    Delete services/logbook directory

--- services/logbook/functions/diving/logbookEnum.js
+++
@@ -1,6 +0,0 @@
-export const Logbook = {
-  diver: 'DIVING_DIVER',
-  supervisor: 'DIVING_SUPERVISOR',
-  inspection: 'DIVING_INSPECTION',
-  inspection_supervisor: 'DIVING_INSPECTION_SUPERVISOR'
-}
