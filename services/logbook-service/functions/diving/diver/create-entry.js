ab920c2dc72214322ff8d3a67bf0626831042442
Sean Knowles <s@seanknowles.dev>
2021-08-15 17:48:13 +0100

    basic email notification functionality built in

--- services/logbook-service/functions/diving/diver/create-entry.js
+++ services/logbook-service/functions/diving/diver/create-entry.js
@@ -2,8 +2,12 @@ import { lambdaHandler, dynamodb, commonMiddleware } from '../../../lib'
 import validator from '@middy/validator'
 import { v4 as uuid } from 'uuid'
 import { Logbook } from '../logbookEnum'
+import AWS from 'aws-sdk'
+
+const sqs = new AWS.SQS()
 
 const LOGBOOK_SERVICE_TABLE = process.env.LOGBOOK_SERVICE_TABLE
+const MAIL_QUEUE_URL = process.env.MAIL_QUEUE_URL
 
 const main = lambdaHandler(async (event) => {
   const { userId } = event.body
@@ -34,7 +38,95 @@ const main = lambdaHandler(async (event) => {
 
   await dynamodb.put(params)
 
-  return params.Item
+  const {
+    entryType,
+    subTypeA,
+    subTypeB,
+    company,
+    client,
+    depthMeters,
+    leftSurface,
+    userVerified,
+    userVerifierId,
+    companyVerified,
+    clientVerified
+  } = params.Item
+
+  const notifyUser = sqs
+    .sendMessage({
+      QueueUrl: MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You have recorded a new logbook entry',
+        recipient: 'sean@opsap.com',
+        body: `
+
+        Hi ${userId},
+
+        This is an automated message.
+
+        You have successfully added a new entry:
+      
+        entryType: ${entryType}
+        subType:   ${subTypeA}
+        subType:   ${subTypeB}
+        Company:   ${company}
+        Client:    ${client}
+        Depth:     ${depthMeters}m 
+        DateTime:  ${leftSurface}
+        
+        User verified?: ${userVerified}
+        Company verified?: ${companyVerified}
+        Client verified?: ${clientVerified}
+
+
+        Our automated verification system has notified ${userVerifierId}. The verifier must digitally sign the entry for the entry to become an official verified record.
+
+        Thanks you.
+
+        OPSAP Team.
+        `
+      })
+    })
+    .promise()
+
+  const notifyVerifier = sqs
+    .sendMessage({
+      QueueUrl: MAIL_QUEUE_URL,
+      MessageBody: JSON.stringify({
+        subject: 'You have recorded a new logbook entry',
+        recipient: 'sean@opsap.com',
+        body: `
+
+        Hi PLACEHOLDER(userVerifierID),
+
+        This is an automated message.
+
+        You have hav been listed as a verifiying party for recorded entry listed below:
+      
+        user:      ${userId}
+        entryType: ${entryType}
+        subType:   ${subTypeA}
+        subType:   ${subTypeB}
+        Company:   ${company}
+        Client:    ${client}
+        Depth:     ${depthMeters}m 
+        DateTime:  ${leftSurface}
+        
+        User verified?: ${userVerified}
+        Company verified?: ${companyVerified}
+        Client verified?: ${clientVerified}
+
+        Please signin into the OPSAP platform to verifiy and validate the record.
+
+        Thanks you.
+
+        OPSAP Team.
+        `
+      })
+    })
+    .promise()
+
+  return Promise.all([notifyUser, notifyVerifier])
 })
 
 const schema = {
--- services/notification-service/functions/send-mail.js
+++ services/notification-service/functions/send-mail.js
@@ -1,4 +1,37 @@
+import AWS from 'aws-sdk'
+
+const ses = new AWS.SES({ region: 'eu-west-1' })
+
 export async function handler(event, context) {
-  console.log(event)
-  return event
+  const record = event.Records[0]
+  console.log('record processing', record)
+
+  const email = JSON.parse(record.body)
+  const { subject, body, recipient } = email
+
+  // params
+  const params = {
+    Source: 'sean@opsap.com',
+    Destination: {
+      ToAddresses: [recipient]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: body
+        }
+      },
+      Subject: {
+        Data: subject
+      }
+    }
+  }
+
+  try {
+    const result = await ses.sendEmail(params).promise()
+    console.log(result)
+    return result
+  } catch (err) {
+    console.error(err)
+  }
 }
