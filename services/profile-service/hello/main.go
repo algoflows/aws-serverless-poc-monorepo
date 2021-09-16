11e4e7e853746df33d04425a1d9818b5c1e65dac
Sean Knowles <s@seanknowles.dev>
2021-09-16 01:35:20 +0100

    added examples for go to single function

--- services/profile-service/hello/main.go
+++ services/profile-service/hello/main.go
@@ -4,23 +4,31 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/epsagon/epsagon-go/epsagon"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+var service_name_env = os.Getenv("service_name_env")
+var epsagon_api_key = os.Getenv("epsagon_api_key")
+
 type Response events.APIGatewayProxyResponse
+type Request events.APIGatewayProxyRequest
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
+func Handler(ctx context.Context, req Request) (Response, error) {
 	var buf bytes.Buffer
+
+	fmt.Println(req.HTTPMethod)
+	fmt.Println(req.Path)
 
 	body, err := json.Marshal(map[string]interface{}{
 		"message": "Go Serverless v1.0! Your function executed successfully!",
+		"testMsg": "testing testing",
 	})
 	if err != nil {
 		return Response{StatusCode: 404}, err
@@ -42,5 +50,6 @@ func Handler(ctx context.Context) (Response, error) {
 }
 
 func main() {
-	lambda.Start(Handler)
+	config := epsagon.NewTracerConfig(service_name_env, epsagon_api_key)
+	lambda.Start(epsagon.WrapLambdaHandler(config, Handler))
 }
--- services/profile-service/world/main.go
+++ services/profile-service/world/main.go
@@ -1,36 +1,58 @@
 package main
 
 import (
-	"bytes"
-	"context"
 	"encoding/json"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/google/uuid"
 )
 
-// Response is of type APIGatewayProxyResponse since we're leveraging the
-// AWS Lambda Proxy Request functionality (default behavior)
-//
-// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+// var service_name_env = os.Getenv("service_name_env")
+// var epsagon_api_key = os.Getenv("epsagon_api_key")
+
 type Response events.APIGatewayProxyResponse
+type Request events.APIGatewayProxyRequest
+
+type Item struct {
+	Id      string `json:"id,omitempty"`
+	Title   string `json:"title"`
+	Details string `json:"details"`
+}
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
+func Handler(request Request) (Response, error) {
 
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Okay so your other function also executed successfully!",
-	})
-	if err != nil {
-		return Response{StatusCode: 404}, err
+	fmt.Println("method", request.HTTPMethod)
+
+	// New uuid for item id
+	itemUuid := uuid.New().String()
+	fmt.Println("Generated new item uuid:", itemUuid)
+
+	// Unmarshal to Item to access object properties
+	itemString := request.Body
+	itemStruct := Item{}
+	json.Unmarshal([]byte(request.Body), &itemStruct)
+
+	if itemStruct.Title == "" {
+		return Response{StatusCode: 400}, nil
 	}
-	json.HTMLEscape(&buf, body)
+
+	// Create new item of type item
+	item := Item{
+		Id:      itemUuid,
+		Title:   itemStruct.Title,
+		Details: itemStruct.Details,
+	}
+
+	// testing
+	fmt.Printf("%s \n %s \n %s", item.Id, item.Title, item.Details)
 
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            buf.String(),
+		Body:            itemString,
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
 			"X-MyCompany-Func-Reply": "world-handler",
@@ -41,5 +63,7 @@ func Handler(ctx context.Context) (Response, error) {
 }
 
 func main() {
+	// config := epsagon.NewTracerConfig(service_name_env, epsagon_api_key)
+	// lambda.Start(epsagon.WrapLambdaHandler(config, Handler))
 	lambda.Start(Handler)
 }
